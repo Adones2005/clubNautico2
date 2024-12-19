b9afa65fdb2a24478cc9694fe11af8d1f047abce
Daniel Adones Garcia <daniel.adones@eviden.com>
2024-12-19 09:02:15 +0100

    Incio proyecto spring clubnautico

---
+++ src/main/java/org/atos/dual/club_nautico_api/ClubNauticoApiApplication.java
@@ -0,0 +1,13 @@
+package org.atos.dual.club_nautico_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubNauticoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubNauticoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/atos/dual/club_nautico_api/ClubNauticoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.atos.dual.club_nautico_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubNauticoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
